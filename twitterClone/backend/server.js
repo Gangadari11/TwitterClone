b35ff52a52d10ad9be7afe5b59e583a3aa10b315
Sansala Gangadari <slicchandrika@gmail.com>
2024-12-28 21:17:31 +0530

    1-auth controllers added

--- twitterClone/backend/server.js
+++ twitterClone/backend/server.js
@@ -2,16 +2,21 @@ import express from 'express';
 import authRoutes from './routes/auth.routes.js';
 import dotenv from 'dotenv';
 import connectMongoDB from './db/connectMongoDB.js';
+import cookieParser from 'cookie-parser';
+
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
-dotenv.config();
 
-app.use('/api/auth',  authRoutes);
+app.use(express.json()); //to parse req.body
+app.use(express.urlencoded({ extended: false })); //to parse from data(urlencoded)
+app.use(cookieParser());
+app.use('/api/auth', authRoutes);
 
-app.get('/', (req, res) => {
-  res.send('Server is ready');
-})
+// app.get('/', (req, res) => {
+//   res.send('Server is ready');
+// })
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
