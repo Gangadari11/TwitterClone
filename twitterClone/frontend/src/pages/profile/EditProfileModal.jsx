7676f45484637ec0b77ac1978ef62150bbcb846e
Sansala Gangadari <slicchandrika@gmail.com>
2025-01-03 08:18:48 +0530

    3-useUpdateProfile hook added to keep the codebase clean

--- twitterClone/frontend/src/pages/profile/EditProfileModal.jsx
+++ twitterClone/frontend/src/pages/profile/EditProfileModal.jsx
@@ -14,40 +14,7 @@ const EditProfileModal = ({authUser}) => {
 	});
 
 	
-	const {mutate:updateProfile , isPending:isUpdateProfile } = useMutation({
-		mutationFn: async() => {
-			try{
-			    const res = await fetch('/api/users/update',{
-					method: 'POST',
-					headers: {
-					    "Content-Type": "application/json",
-
-					},
-					body: JSON.stringify({
-					    coverImg,
-						profileImg,
-					}),
-				})
-				const data = await res.json();
-				if(!res.ok){
-				    throw  new Error(data.error || "Something went wrong");
-				}
-				return data;
-			}catch(error){
-				throw new Error(error);
-			}
-		},
-		onSuccess: () => {
-			toast.success("Profile updated successfully");
-			Promise.all([
-				queryClient.invalidateQueries({queryKey: ['authUser']}),
-				queryClient.invalidateQueries({queryKey: ['userProfile']}),
-			])
-		},
-		onError: (error) => {
-			toast.error(error.message);
-		}
-	})
+	const {updateProfile , isUpdatingProfile} =useUpdateUserProfile();
 
 	const handleInputChange = (e) => {
 		setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -83,7 +50,7 @@ const EditProfileModal = ({authUser}) => {
 						className='flex flex-col gap-4'
 						onSubmit={(e) => {
 							e.preventDefault();
-							updateProfile();
+							updateProfile(formData);
 						}}
 					>
 						<div className='flex flex-wrap gap-2'>
--- twitterClone/frontend/src/pages/profile/ProfilePage.jsx
+++ twitterClone/frontend/src/pages/profile/ProfilePage.jsx
@@ -50,40 +50,7 @@ const ProfilePage = () => {
 	})
 
 
-	const {mutate:updateProfile , isPending:isUpdateProfile } = useMutation({
-		mutationFn: async() => {
-			try{
-			    const res = await fetch('/api/users/update',{
-					method: 'POST',
-					headers: {
-					    "Content-Type": "application/json",
-
-					},
-					body: JSON.stringify({
-					    coverImg,
-						profileImg,
-					}),
-				})
-				const data = await res.json();
-				if(!res.ok){
-				    throw  new Error(data.error || "Something went wrong");
-				}
-				return data;
-			}catch(error){
-				throw new Error(error);
-			}
-		},
-		onSuccess: () => {
-			toast.success("Profile updated successfully");
-			Promise.all([
-				queryClient.invalidateQueries({queryKey: ['authUser']}),
-				queryClient.invalidateQueries({queryKey: ['userProfile']}),
-			])
-		},
-		onError: (error) => {
-			toast.error(error.message);
-		}
-	})
+	const {isUpdatingProfile , updateProfile} = useMutation()
 	const isMyProfile = authUser._id === user?._id; 
 	const memberSinceDate = formatMemberSinceDate(user?.createdAt);
 	const amIFollowing = authUser?.following.includes(user?._id);
@@ -182,7 +149,11 @@ const ProfilePage = () => {
 								{(coverImg || profileImg) && (
 									<button
 										className='btn btn-primary rounded-full btn-sm text-white px-4 ml-2'
-										onClick={() => updateProfile()}
+										onClick={async () => {
+											await updateProfile({coverImg , profileImg})
+											setCoverImg(null);
+											setProfileImg(null);
+										}}
 									>
 										{isUpdateProfile ? "Updating..." : "Update"}
 									</button>
